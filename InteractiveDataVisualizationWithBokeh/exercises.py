ce9a81361128ca3494e9fa187decb680b2586d02
llwang8 <wangxbl56@gmail.com>
2017-05-23 11:25:24 -0400

    finish Data Scientist with Python Tranck and certificate

--- InteractiveDataVisualizationWithBokeh/exercises.py
+++ InteractiveDataVisualizationWithBokeh/exercises.py
@@ -1,6 +1,8 @@
 # DataCamp
 # Interactive Data Visualization with Bokeh
 
+# 1. Basic plotting with Bokeh
+
 # Plotting with glyphs
 
 # Import figure from bokeh.plotting
@@ -159,5 +161,439 @@ p.circle('weight', 'mpg', source=source,
 output_file('colormap.html')
 show(p)
 
+
+
+# 2. Layouts, Interaction, and Annotation
+
+# Creating rows of plots
+# Import row from bokeh.layouts
+from bokeh.layouts import row
+# Create the first figure: p1
+p1 = figure(x_axis_label='fertility (children per woman)', y_axis_label='female_literacy (% population)')
+# Add a circle glyph to p1
+p1.circle('fertility', 'female_literacy', source=source)
+# Create the second figure: p2
+p2 = figure(x_axis_label='population', y_axis_label='female_literacy (% population)')
+# Add a circle glyph to p2
+p2.circle('population', 'female_literacy', source=source)
+# Put p1 and p2 into a horizontal row: layout
+layout = row(p1, p2)
+# Specify the name of the output_file and show the result
+output_file('fert_row.html')
+show(layout)
+
+
+# Creating columns of plots
+# Import column from the bokeh.layouts module
+from bokeh.layouts import column
+# Create a blank figure: p1
+p1 = figure(x_axis_label='fertility (children per woman)', y_axis_label='female_literacy (% population)')
+# Add circle scatter to the figure p1
+p1.circle('fertility', 'female_literacy', source=source)
+# Create a new blank figure: p2
+p2 = figure(x_axis_label='population', y_axis_label='female_literacy (% population)')
+# Add circle scatter to the figure p2
+p2.circle('population', 'female_literacy', source=source)
+# Put plots p1 and p2 in a column: layout
+layout = column(p1, p2)
+# Specify the name of the output_file and show the result
+output_file('fert_column.html')
+show(layout)
+
+
+# Nesting rows and columns
+# Import column and row from bokeh.layouts
+from bokeh.layouts import row, column
+# Make a column layout that will be used as the second row: row2
+row2 = column([mpg_hp, mpg_weight], sizing_mode='scale_width')
+# Make a row layout that includes the above column layout: layout
+layout = row([avg_mpg, row2], sizing_mode='scale_width')
+# Specify the name of the output_file and show the result
+output_file('layout_custom.html')
+show(layout)
+
+
+# Creating gridded layouts
+
+# Import gridplot from bokeh.layouts
+from bokeh.layouts import gridplot
+# Create a list containing plots p1 and p2: row1
+row1 = [p1, p2]
+# Create a list containing plots p3 and p4: row2
+row2 = [p3, p4]
+# Create a gridplot using row1 and row2: layout
+layout = gridplot([row1, row2])
+# Specify the name of the output_file and show the result
+output_file('grid.html')
+show(layout)
+
+
+# Starting tabbed layouts
+# Import Panel from bokeh.models.widgets
+from bokeh.models.widgets import Panel
+# Create tab1 from plot p1: tab1
+tab1 = Panel(child=p1, title='Latin America')
+# Create tab2 from plot p2: tab2
+tab2 = Panel(child=p2, title='Africa')
+# Create tab3 from plot p3: tab3
+tab3 = Panel(child=p3, title='Asia')
+# Create tab4 from plot p4: tab4
+tab4 = Panel(child=p4, title='Europe')
+
+
+# Import Tabs from bokeh.models.widgets
+from bokeh.models.widgets import Tabs
+# Create a Tabs layout: layout
+layout = Tabs(tabs=[tab1, tab2, tab3, tab4])
+# Specify the name of the output_file and show the result
+output_file('tabs.html')
+show(layout)
+
+
+# Linked axes
+
+# Link the x_range of p2 to p1: p2.x_range
+p2.x_range = p1.x_range
+# Link the y_range of p2 to p1: p2.y_range
+p2.y_range = p1.y_range
+# Link the x_range of p3 to p1: p3.x_range
+p3.x_range = p1.x_range
+# Link the y_range of p4 to p1: p4.y_range
+p4.y_range = p1.y_range
+# Specify the name of the output_file and show the result
+output_file('linked_range.html')
+show(layout)
+
+
+# Linked Brushing
+
+# Add a circle glyph to p1
+p1.circle('fertility', 'female literacy', source=source)
+# Create the second figure: p2
+p2 = figure(x_axis_label='fertility (children per woman)', y_axis_label='population (millions)',
+            source=source)
+# Add a circle glyph to p2
+p2.circle('fertility', 'population', source=source)
+# Create row layout of figures p1 and p2: layout
+layout = row(p1, p2)
+
+
+# Create ColumnDataSource: source
+source = ColumnDataSource(data)
+
+# Create the first figure: p1
+p1 = figure(x_axis_label='fertility (children per woman)', y_axis_label='female literacy (% population)', tools='box_select,lasso_select')
+# Add a circle glyph to p1
+p1.circle('fertility', 'female literacy', source=source)
+# Create the second figure: p2
+p2 = figure(x_axis_label='fertility (children per woman)', y_axis_label='population (millions)', tools='box_select,lasso_select')
+# Add a circle glyph to p2
+p2.circle('fertility', 'population', source=source)
+# Create row layout of figures p1 and p2: layout
+layout = row(p1, p2)
+# Specify the name of the output_file and show the result
+output_file('linked_brush.html')
+show(layout)
+
+
+# How to create legends
+# Add the first circle glyph to the figure p
+p.circle('fertility', 'female_literacy', source=latin_america, size=10, color='red', legend='Latin America')
+# Add the second circle glyph to the figure p
+p.circle('fertility', 'female_literacy', source=africa, size=10, color='blue', legend='Africa')
+# Specify the name of the output_file and show the result
+output_file('fert_lit_groups.html')
+show(p)
+
+
+# Positioning and styling legends
+# Assign the legend to the bottom left: p.legend.location
+p.legend.location = 'bottom_left'
+# Fill the legend background with the color 'lightgray': p.legend.background_fill_color
+p.legend.background_fill_color = 'lightgray'
+# Specify the name of the output_file and show the result
+output_file('fert_lit_groups.html')
+show(p)
+
+
+# Adding a hover tooltip
+# Import HoverTool from bokeh.models
+from bokeh.models import HoverTool
+# Create a HoverTool object: hover
+hover = HoverTool(tooltips=[('Country', '@Country')])
+# Add the HoverTool object to figure p
+p.add_tools(hover)
+# Specify the name of the output_file and show the result
+output_file('hover.html')
+show(p)
+
+
+# High Level Graphs
+
+# A basic histogram
+# Import Histogram, output_file, and show from bokeh.charts
+from bokeh.charts import Histogram, output_file, show
+# Make a Histogram: p
+p = Histogram(df, 'female_literacy', title='Female Literacy')
+# Set the x axis label
+p.xaxis.axis_label = 'Female Literacy'
+# Set the y axis label
+p.yaxis.axis_label = 'Frequency'
+# Specify the name of the output_file and show the result
+output_file('histogram.html')
+show(p)
+
+
+# Controlling the number of bins
+
+# Import Histogram, output_file, and show from bokeh.charts
+from bokeh.charts import Histogram, output_file, show
+# Make the Histogram: p
+p = Histogram(df, 'female_literacy', title='Female Literacy', bins=40)
+# Set axis labels
+p.xaxis.axis_label = 'Female Literacy (% population)'
+p.yaxis.axis_label = 'Number of Countries'
+# Specify the name of the output_file and show the result
+output_file('histogram.html')
+show(p)
+
+
+# Generating multiple histograms at once
+# Import Histogram, output_file, and show from bokeh.charts
+from bokeh.charts import Histogram, output_file, show
+# Make a Histogram: p
+p = Histogram(df, 'female_literacy', title='Female Literacy',
+              color='Continent', legend='top_left')
+# Set axis labels
+p.xaxis.axis_label = 'Female Literacy (% population)'
+p.yaxis.axis_label = 'Number of Countries'
+# Specify the name of the output_file and show the result
+output_file('hist_bins.html')
+show(p)
+
+
+# A basic box plot
+# Import BoxPlot, output_file, and show from bokeh.charts
+from bokeh.charts import BoxPlot, output_file, show
+# Make a box plot: p
+p = BoxPlot(df, values='female_literacy', label='Continent',
+            title='Female Literacy (grouped by Continent)', legend='bottom_right')
+# Set the y axis label
+p.yaxis.axis_label = 'Female literacy (% population)'
+# Specify the name of the output_file and show the result
+output_file('boxplot.html')
+show(p)
+
+
+# Color different groups differently
+# Import BoxPlot, output_file, and show
+from bokeh.charts import BoxPlot, output_file, show
+# Make a box plot: p
+p = BoxPlot(df, values='female_literacy', label='Continent', color='Continent',
+            title='Female Literacy (grouped by Continent)', legend='bottom_right')
+# Set y-axis label
+p.yaxis.axis_label = 'Female literacy (% population)'
+# Specify the name of the output_file and show the result
+output_file('boxplot.html')
+show(p)
+
+
+# A basic scatter plot
+# Import Scatter, output_file, and show from bokeh.charts
+from bokeh.charts import Scatter, output_file, show
+# Make a scatter plot: p
+p = Scatter(df, x='population', y='female_literacy',
+            title='Female Literacy vs Population')
+# Set the x-axis label
+p.xaxis.axis_label = 'population'
+# Set the y-axis label
+p.yaxis.axis_label = 'female_literacy'
+# Specify the name of the output_file and show the result
+output_file('scatterplot.html')
+show(p)
+
+
+# Using colors to group data
+# Import Scatter, output_file, and show from bokeh.charts
+from bokeh.charts import Scatter, output_file, show
+# Make a scatter plot such that each circle is colored by its continent: p
+p = Scatter(df, x='population', y='female_literacy', color='Continent',
+            title='Female Literacy vs Population')
+# Set x-axis and y-axis labels
+p.xaxis.axis_label = 'Population (millions)'
+p.yaxis.axis_label = 'Female literacy (% population)'
+# Specify the name of the output_file and show the result
+output_file('scatterplot.html')
+show(p)
+
+
+# Using shapes to group data
+# Import Scatter, output_file, and show from bokeh.charts
+from bokeh.charts import Scatter, output_file, show
+# Make a scatter plot such that each continent has a different marker type: p
+p = Scatter(df, x='population', y='female_literacy', color='Continent', marker='Continent', title='Female Literacy vs Population')
+# Set x-axis and y-axis labels
+p.xaxis.axis_label = 'Population (millions)'
+p.yaxis.axis_label = 'Female literacy (% population)'
+# Specify the name of the output_file and show the result
+output_file('scatterplot.html')
+show(p)
+
+
+# 4. Building interactive apps with Bokeh Server
+
+# Using the current document
+# Perform necessary imports
+from bokeh.io import curdoc
+from bokeh.plotting import figure
+# Create a new plot: plot
+plot = figure()
+# Add a line to the plot
+plot.line([1,2,3,4,5], [2,5,4,6,7])
+# Add the plot to the current document
+curdoc().add_root(plot)
+
+
+# Add a single slider
+# Perform the necessary imports
+from bokeh.io import curdoc
+from bokeh.layouts import widgetbox
+from bokeh.models import Slider
+# Create a slider: slider
+slider = Slider(title='my slider', start=0, end=10, step=0.1, value=2)
+# Create a widgetbox layout: layout
+layout = widgetbox(slider)
+# Add the layout to the current document
+curdoc().add_root(layout)
+
+
+# Multiple sliders in one document
+# Perform necessary imports
+from bokeh.io import curdoc
+from bokeh.layouts import widgetbox
+from bokeh.models import Slider
+# Create first slider: slider1
+slider1 = Slider(title='slider1', start=0, end=10, step=0.1, value=2)
+# Create second slider: slider2
+slider2 = Slider(title='slider2', start=10, end=100, step=1, value=20)
+# Add slider1 and slider2 to a widgetbox
+layout = widgetbox(slider1, slider2)
+# Add the layout to the current document
+curdoc().add_root(layout)
+
+
+# How to combine Bokeh models into layouts
+# Create ColumnDataSource: source
+source = ColumnDataSource(data={'x': x, 'y': y})
+# Add a line to the plot
+plot.line(x='x', y='y', source=source)
+# Create a column layout: layout
+layout = column(widgetbox(slider), plot)
+# Add the layout to the current document
+curdoc().add_root(layout)
+
+
+# learn about widgetbox callbacks
+# Define a callback function: callback
+def callback(attr, old, new):
+    # Read the current value of the slider: scale
+    scale = slider.value
+    # Compute the updated y using np.sin(scale/x): new_y
+    new_y = np.sin(scale/x)
+    # Update source with the new data values
+    source.data = {'x': x, 'y': new_y}
+# Attach the callback to the 'value' property of slider
+slider.on_change('value', callback)
+
+
+# updating data sources from dropdown callbacks
+
+# Perform necessary imports
+from bokeh.models import ColumnDataSource, Select
+# Create ColumnDataSource: source
+source = ColumnDataSource(data={
+    'x' : fertility,
+    'y' : female_literacy
+})
+# Create a new plot: plot
+plot = figure()
+# Add circles to the plot
+plot.circle('x', 'y', source=source)
+# Define a callback function: update_plot
+def update_plot(attr, old, new):
+    # If the new Selection is 'female_literacy', update 'y' to female_literacy
+    if new == 'female_literacy':
+        source.data = {
+            'x' : fertility,
+            'y' : female_literacy
+        }
+    # Else, update 'y' to population
+    else:
+        source.data = {
+            'x' : fertility,
+            'y' : population
+        }
+# Create a dropdown Select widget: select
+select = Select(title="distribution", options=['female_literacy', 'population'], value='female_literacy')
+# Attach the update_plot callback to the 'value' property of select
+select.on_change('value', update_plot)
+# Create layout and add to current document
+layout = row(select, plot)
+curdoc().add_root(layout)
+
+
+# Synthronoze two dropdowns
+# Create two dropdown Select widgets: select1, select2
+select1 = Select(title='First', options=['A', 'B'], value='A')
+select2 = Select(title='Second', options=['1', '2', '3'], value='1')
+
+# Define a callback function: callback
+def callback(attr, old, new):
+    # If select1 is 'A'
+    if select1.value == 'A':
+        # Set select2 options to ['1', '2', '3']
+        select2.options = ['1', '2', '3']
+
+        # Set select2 value to '1'
+        select2.value = '1'
+    else:
+        # Set select2 options to ['100', '200', '300']
+        select2.options = ['100', '200', '300']
+        # Set select2 value to '100'
+        select2.value = '100'
+
+# Attach the callback to the 'value' property of select1
+select1.on_change('value', callback)
+
+# Create layout and add to current document
+layout = widgetbox(select1, select2)
+curdoc().add_root(layout)
+
+
+# Button widgets
+# Create a Button with label 'Update Data'
+button = Button(label='Update Data')
+# Define an update callback with no arguments: update
+def update():
+    # Compute new y values: y
+    y = np.sin(x) + np.random.random(N)
+    # Update the ColumnDataSource data dictionary
+    source.data['y'] = y
+# Add the update callback to the button
+button.on_click(update)
+
+
+
+# Button styles
+# Import CheckboxGroup, RadioGroup, Toggle from bokeh.models
+from bokeh.models import CheckboxGroup, RadioGroup, Toggle
+# Add a Toggle: toggle
+toggle = Toggle(label='Toggle button', button_type='success')
+# Add a CheckboxGroup: checkbox
+checkbox = CheckboxGroup(labels=['Option 1', 'Option 2', 'Option 3'])
+# Add a RadioGroup: radio
+radio = RadioGroup(labels=['Option 1', 'Option 2', 'Option 3'])
+# Add widgetbox(toggle, checkbox, radio) to the current document
+curdoc().add_root(widgetbox(toggle, checkbox, radio))
 
 
---
+++ IntroToNetworks/exercise.py
@@ -0,0 +1,385 @@
+# DataCamp
+# Network Analysis in Python
+
+# Exercises
+
+# 1. Intro to Networks
+
+# Basics of NetworkX API, using Twitter network
+# What is the size of the graph T,
+# ? the type of T.nodes(),
+# ? the data structure of the third element of the last entry of
+# T.edges(data=True)?
+len(T)
+# 23369
+type(T.nodes())
+#list
+T.edges(data=True)[-1]
+# (23324, 23327, {'date': datetime.date(2008, 2, 9)})
+
+# Basic drawing of a network using NetworkX
+# Import necessary modules
+import matplotlib.pyplot as plt
+import networkx as nx
+# Draw the graph to screen
+nx.draw(T_sub)
+plt.show()
+
+# Queries on a graph
+# Use a list comprehension to get the nodes of interest: noi
+noi = [n for n, d in T.nodes(data=True) if d['occupation'] == 'scientist']
+# Use a list comprehension to get the edges of interest: eoi
+eoi = [(u, v) for u, v, d in T.edges(data=True) if d['date'] < date(2010, 1, 1)]
+
+
+# specifying a weight on edges
+# Set the weight of the edge
+weight = 2
+# Iterate over all the edges (with metadata)
+for u, v, d in T.edges(data=True):
+    # Check if node 293 is involved
+    if 293 in T.edges(data=True):
+        # Set the weight to 1.1
+        d['weight'] = 1.1
+
+
+# Set the weight of the edge
+T.edge[1][10]['weight'] = 2
+# Iterate over all the edges (with metadata)
+for u, v, d in T.edges(data=True):
+    # Check if node 293 is involved
+    if 293 in [u, v]:
+        # Set the weight to 1.1
+        T[u][v]['weight'] = 1.1
+
+
+# Checking whether there are self-loops in the graph
+# Define find_selfloop_nodes()
+def find_selfloop_nodes(G):
+    """
+    Finds all nodes that have self-loops in the graph G.
+    """
+    nodes_in_selfloops = []
+    # Iterate over all the edges of G
+    for u, v in G.edges():
+    # Check if node u and node v are the same
+        if u == v:
+            # Append node u to nodes_in_selfloops
+            nodes_in_selfloops.append(u)
+    return nodes_in_selfloops
+# Check whether number of self loops equals the number of nodes in self loops
+assert T.number_of_selfloops() == len(find_selfloop_nodes(T))
+
+
+#Visualizing using Matrix plots
+# Import nxviz
+import nxviz as nv
+# Create the MatrixPlot object: m
+m = nv.MatrixPlot(T)
+# Draw m to the screen
+m.draw()
+# Display the plot
+plt.show()
+# Convert T to a matrix format: A
+A = nx.to_numpy_matrix(T)
+# Convert A back to the NetworkX form as a directed graph: T_conv
+T_conv = nx.from_numpy_matrix(A, create_using=nx.DiGraph())
+# Check that the `category` metadata field is lost from each node
+for n, d in T_conv.nodes(data=True):
+    assert 'category' not in d.keys()
+
+
+# Visualizing using circos plots
+# Import necessary modules
+import matplotlib.pyplot as plt
+from nxviz import CircosPlot
+# Create the CircosPlot object: c
+c = CircosPlot(T)
+# Draw c to the screen
+c.draw()
+
+# Display the plot
+plt.show()
+
+
+# visualizing using Arc plots
+# Import necessary modules
+import matplotlib.pyplot as plt
+from nxviz import ArcPlot
+# Create the un-customized ArcPlot object: a
+a = ArcPlot(T)
+# Draw a to the screen
+a.draw()
+# Display the plot
+plt.show()
+# Create the customized ArcPlot object: a2
+a2 = ArcPlot(T, node_order='category', node_color='category')
+# Draw a2 to the screen
+a2.draw()
+# Display the plot
+plt.show()
+
+
+# 2. Important nodes
+
+# compute number of neighbors for each node
+# Define nodes_with_m_nbrs()
+def nodes_with_m_nbrs(G, m):
+    """
+    Returns all nodes in graph G that have m neighbors.
+    """
+    nodes = set()
+    # Iterate over all nodes in G
+    for n in G.nodes():
+        # Check if the number of neighbors of n matches m
+        if len(G.neighbors(n)) == m:
+            # Add the node n to the set
+            nodes.add(n)
+    # Return the nodes with m neighbors
+    return nodes
+# Compute and print all nodes in T that have 6 neighbors
+six_nbrs = nodes_with_m_nbrs(T, 6)
+print(six_nbrs)
+
+
+# Compute degree distribution
+# Compute the degree of every node: degrees
+degrees = [len(T.neighbors(n)) for n in T.nodes()]
+# Print the degrees
+print(degrees)
+
+
+# Degree centrality distribution
+# Import matplotlib.pyplot
+import matplotlib.pyplot as plt
+# Compute the degree centrality of the Twitter network: deg_cent
+deg_cent = nx.degree_centrality(T)
+# Plot a histogram of the degree centrality distribution of the graph.
+plt.figure()
+plt.hist(list(deg_cent.values()))
+plt.show()
+# Plot a histogram of the degree distribution of the graph
+plt.figure()
+plt.hist(degrees)
+plt.show()
+# Plot a scatter plot of the centrality distribution and the degree distribution
+plt.figure()
+plt.scatter(degrees, list(deg_cent.values()))
+plt.show()
+
+
+# shortest Path
+
+# Define path_exists()
+def path_exists(G, node1, node2):
+    """
+    This function checks whether a path exists between two nodes (node1, node2) in graph G.
+    """
+    visited_nodes = set()
+    # Initialize the queue of cells to visit with the first node: queue
+    queue = [node1]
+    # Iterate over the nodes in the queue
+    for node in queue:
+        # Get neighbors of the node
+        neighbors = G.neighbors(node)
+        # Check to see if the destination node is in the set of neighbors
+        if node2 in neighbors:
+            print('Path exists between nodes {0} and {1}'.format(node1, node2))
+            return True
+            break
+        else:
+            # Add current node to visited nodes
+            visited_nodes.add(node)
+            # Add neighbors of current node that have not yet been visited
+            queue.extend([n for n in neighbors if n not in visited_nodes])
+        # Check to see if the final element of the queue has been reached
+        if node == queue[-1]:
+            print('Path does not exist between nodes {0} and {1}'.format(node1, node2))
+            # Place the appropriate return statement
+            return False
+
+# NetworkX betweenness centrality on a social network
+# Compute the betweenness centrality of T: bet_cen
+bet_cen = nx.betweenness_centrality(T)
+# Compute the degree centrality of T: deg_cen
+deg_cen = nx.degree_centrality(T)
+# Create a scatter plot of betweenness centrality and degree centrality
+plt.scatter(list(bet_cen.values()), list(deg_cen.values()) )
+# Display the plot
+plt.show()
+
+
+# Deep Dive - Twitter newtwork
+
+# Define find_nodes_with_highest_deg_cent()
+def find_nodes_with_highest_deg_cent(G):
+    # Compute the degree centrality of G: deg_cent
+    deg_cent = nx.degree_centrality(G)
+    # Compute the maximum degree centrality: max_dc
+    max_dc = max(list(deg_cent.values()))
+    nodes = set()
+    # Iterate over the degree centrality dictionary
+    for k, v in deg_cent.items():
+        # Check if the current value has the maximum degree centrality
+        if v == max_dc:
+            # Add the current node to the set of nodes
+            nodes.add(k)
+    return nodes
+# Find the node(s) that has the highest degree centrality in T: top_dc
+top_dc = find_nodes_with_highest_deg_cent(T)
+print(top_dc)
+# Write the assertion statement
+for node in top_dc:
+    assert nx.degree_centrality(T)[node] == max(nx.degree_centrality(T).values())
+
+
+# Define find_node_with_highest_bet_cent()
+def find_node_with_highest_bet_cent(G):
+    # Compute betweenness centrality: bet_cent
+    bet_cent = nx.betweenness_centrality(G)
+    # Compute maximum betweenness centrality: max_bc
+    max_bc = max(list(bet_cent.values()))
+    nodes = set()
+    # Iterate over the betweenness centrality dictionary
+    for k, v in bet_cent.items():
+        # Check if the current value has the maximum betweenness centrality
+        if v == max_bc:
+            # Add the current node to the set of nodes
+            nodes.add(k)
+    return nodes
+# Use that function to find the node(s) that has the highest betweenness centrality in the network: top_bc
+top_bc = find_node_with_highest_bet_cent(T)
+# Write an assertion statement that checks that the node(s) is/are correctly identified.
+for node in top_bc:
+    assert nx.betweenness_centrality(T)[node] == max(nx.betweenness_centrality(T).values())
+
+
+# 3. Structures
+
+# Identifying triangle relationships
+from itertools import combinations
+# Define is_in_triangle()
+def is_in_triangle(G, n):
+    """
+    Checks whether a node `n` in graph `G` is in a triangle relationship or not.
+    Returns a boolean.
+    """
+    in_triangle = False
+    # Iterate over all possible triangle relationship combinations
+    for n1, n2 in combinations(G.neighbors(n), 2):
+        # Check if an edge exists between n1 and n2
+        if G.has_edge(n1, n2):
+            in_triangle = True
+            break
+    return in_triangle
+
+
+# Finding modes involved in triangles
+from itertools import combinations
+# Write a function that identifies all nodes in a triangle relationship with a given node.
+def nodes_in_triangle(G, n):
+    """
+    Returns the nodes in a graph `G` that are involved in a triangle relationship with the node `n`.
+    """
+    triangle_nodes = set([n])
+    # Iterate over all possible triangle relationship combinations
+    for n1, n2 in combinations(G.neighbors(n), 2):
+        # Check if n1 and n2 have an edge between them
+        if G.has_edge(n1, n2):
+            # Add n1 to triangle_nodes
+            triangle_nodes.add(n1)
+            # Add n2 to triangle_nodes
+            triangle_nodes.add(n2)
+    return triangle_nodes
+# Write the assertion statement
+assert len(nodes_in_triangle(T, 1)) == 35
+
+
+# Finding open triangles
+from itertools import combinations
+# Define node_in_open_triangle()
+def node_in_open_triangle(G, n):
+    """
+    Checks whether pairs of neighbors of node `n` in graph `G` are in an 'open triangle' relationship with node `n`.
+    """
+    in_open_triangle = False
+    # Iterate over all possible triangle relationship combinations
+    for n1, n2 in combinations(G.neighbors(n), 2):
+        # Check if n1 and n2 do NOT have an edge between them
+        if not G.has_edge(n1, n2):
+            in_open_triangle = True
+            break
+    return in_open_triangle
+# Compute the number of open triangles in T
+num_open_triangles = 0
+# Iterate over all the nodes in T
+for n in T.nodes():
+    # Check if the current node is in an open triangle
+    if node_in_open_triangle(T, n):
+        # Increment num_open_triangles
+        num_open_triangles += 1
+print(num_open_triangles)
+
+
+# Finding all maximal cliques of size 'n'
+# Define maximal_cliques()
+def maximal_cliques(G, size):
+    """
+    Finds all maximal cliques in graph `G` that are of size `size`.
+    """
+    mcs = []
+    for clique in nx.find_cliques(G):
+        if len(clique) == size:
+            mcs.append(clique)
+    return mcs
+# Check that there are 33 maximal cliques of size 3 in the graph T
+assert len(maximal_cliques(T, 3)) == 33
+
+
+# Subgraphs
+
+nodes_of_interest = [29, 38, 42]
+# Define get_nodes_and_nbrs()
+def get_nodes_and_nbrs(G, nodes_of_interest):
+    """
+    Returns a subgraph of the graph `G` with only the `nodes_of_interest` and their neighbors.
+    """
+    nodes_to_draw = []
+    # Iterate over the nodes of interest
+    for n in nodes_of_interest:
+        # Append the nodes of interest to nodes_to_draw
+        nodes_to_draw.append(n)
+        # Iterate over all the neighbors of node n
+        for nbr in G.neighbors(n):
+            # Append the neighbors of n to nodes_to_draw
+            nodes_to_draw.append(nbr)
+    return G.subgraph(nodes_to_draw)
+# Extract the subgraph with the nodes of interest: T_draw
+T_draw = get_nodes_and_nbrs(T, nodes_of_interest)
+# Draw the subgraph to the screen
+nx.draw(T_draw)
+plt.show()
+
+
+# Extract the nodes of interest: nodes
+nodes = [n for n, d in T.nodes(data=True) if d['occupation'] == 'celebrity']
+# Create the set of nodes: nodeset
+nodeset = set(nodes)
+# Iterate over nodes
+for n in nodes:
+    # Compute the neighbors of n: nbrs
+    nbrs = T.neighbors(n)
+    # Compute the union of nodeset and nbrs: nodeset
+    nodeset = nodeset.union(nbrs)
+# Compute the subgraph using nodeset: T_sub
+T_sub = T.subgraph(nodeset)
+# Draw T_sub to the screen
+nx.draw(T_sub)
+plt.show()
+
+
+
+
+# 4. Bringing it all together
+
+
